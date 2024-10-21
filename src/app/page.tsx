65971f11296becb045c394b13cb5f18de1e18597
aurangzaibmughal <162279456+aurangzaibmughal@users.noreply.github.com>
2024-10-21 18:08:41 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-center bg-blue-500 mt:100">Hello World!</h1>
+      <div>
+      <h2 className=" text-center bg-red-700 ">My First website on NextJs</h2></div>
+      <div>
+      <h3 className="text-center bg-amber-600">Thanks to Governor Sindh Kamran Khan Tessori's Initiative for Artifcial Intelligence,WEB 3.0 & Metaverse</h3></div>
+      <div className="text-center bg-slate-500">THANKS</div>
+      
+    </div>
+  
+  )
+};
+  
